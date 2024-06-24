de8cde7eaaf15434b67e880566ea555b7d0bd64b
VictorColman24 <vcolman2010@gmail.com>
2024-06-24 20:08:48 -0300

    Se agrega paginador con la opcion de que el usuario eliga la cantidad de imagenes por pagina, se modifican los templates para mejorar la imagen.

--- nasa_image_gallery/layers/services/services_nasa_image_gallery.py
+++ nasa_image_gallery/layers/services/services_nasa_image_gallery.py
@@ -7,12 +7,9 @@ from django.contrib.auth import get_user
 from django.shortcuts import redirect
 
 def getAllImages(input=None):
-   # obtiene un listado de imágenes desde transport.py y lo guarda en un json_collection.
-    # ¡OJO! el parámetro 'input' indica si se debe buscar por un valor introducido en el buscador.
     json_collection = transport.getAllImages(input)
     images = []
-    # recorre el listado de objetos JSON, lo transforma en una NASACard y lo agrega en el listado de images. Ayuda: ver mapper.py.
-    for json in json_collection:
+    for json in json_collection: # recorre el listado de objetos JSON, lo transforma en una NASACard y lo agrega en el listado de images
         try: # se agrega un try para que en caso de que falte algun item en la creación de nasa card no tire error
             imagen=mapper.fromRequestIntoNASACard(json)
             if imagen.title and imagen.description and imagen.image_url and imagen.date:
--- nasa_image_gallery/views.py
+++ nasa_image_gallery/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import redirect, render
 from .layers.services import services_nasa_image_gallery
 from django.contrib.auth.decorators import login_required # type: ignore
 from django.contrib.auth import logout
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # función que invoca al template del índice de la aplicación.
 def index_page(request):
@@ -14,30 +15,52 @@ def index_page(request):
 def getAllImagesAndFavouriteList(request):
     favourite_list =services_nasa_image_gallery.getAllFavouritesByUser(request)#llama a la funcion solo para traer los favoritos
     images = services_nasa_image_gallery.getAllImages()#llama a la funcion que trae todas las imagenes  
-    return images, favourite_list
+    paginator = Paginator(images, 5)  # Mostrar 5 imágenes por página
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'home.html', {'page_obj': page_obj, 'favourite_list': favourite_list})
 
 # función principal de la galería.
 def home(request):
-    # llama a la función auxiliar getAllImagesAndFavouriteList() y obtiene 2 listados: uno de las imágenes de la API y otro de favoritos por usuario*.
-    # (*) este último, solo si se desarrolló el opcional de favori  tos; caso contrario, será un listado vacío [].
     images = services_nasa_image_gallery.getAllImages()
     favourite_list = services_nasa_image_gallery.getAllFavouritesByUser(request)
-    return render(request, 'home.html', {'images': images, 'favourite_list': favourite_list} )
+    images_per_page = int(request.GET.get('per_page', 5))  # Default 5 imágenes por página
+    paginator = Paginator(images, images_per_page)
+    page_number = request.GET.get('page')
+    try:
+        page_obj = paginator.page(page_number)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        page_obj = paginator.page(paginator.num_pages)
 
+    return render(request, 'home.html', {'page_obj': page_obj, 'favourite_list': favourite_list, 'images_per_page': images_per_page})
 
 # función utilizada en el buscador.
 def search(request):
-    images, favourite_list = getAllImagesAndFavouriteList(request)
-    search_msg = request.POST.get('query', '')
-    if not search_msg:
-        images=services_nasa_image_gallery.getAllImages(None)
+    search_msg = request.GET.get('query', '')
+    images = []
+
+    if search_msg:
+        images = services_nasa_image_gallery.getImagesBySearchInputLike(search_msg)
     else:
-        images=services_nasa_image_gallery.getImagesBySearchInputLike(search_msg) # ver
-    # si el usuario no ingresó texto alguno, debe refrescar la página; caso contrario, debe filtrar aquellas imágenes que posean el texto de búsqueda.
-    return render(request, 'home.html', {'images': images, 'favourite_list': favourite_list} ) # ver
+        images = services_nasa_image_gallery.getAllImages(None)
+        #CODIGO PARA COLOCAR PAGINAS EN LA APP
+    images_per_page = int(request.GET.get('per_page', 5))  #  avariable per_page que proviene del url
 
+    paginator = Paginator(images, images_per_page) # Funcion paginatos images=total de imagenes, images_per_page= imagenes por pagina   
+    page_number = request.GET.get('page')
+    
+    try: 
+        page_obj = paginator.page(page_number)
+    except PageNotAnInteger: #Error en caso de que no sea  un entero
+        page_obj = paginator.page(1) #Ir a pagina 1
+    except EmptyPage: # En caso de que la pagina este vacía
+        page_obj = paginator.page(paginator.num_pages) 
 
-# las siguientes funciones se utilizan para implementar la sección de favoritos: traer los favoritos de un usuario, guardarlos, eliminarlos y desloguearse de la app.
+    return render(request, 'home.html', {'page_obj': page_obj, 'query': search_msg, 'images_per_page': images_per_page})
+
 @login_required
 def getAllFavouritesByUser(request):
     favourite_list=services_nasa_image_gallery.getAllFavouritesByUser(request)
@@ -47,8 +70,8 @@ def getAllFavouritesByUser(request):
 @login_required
 def saveFavourite(request):
     services_nasa_image_gallery.saveFavourite(request)
-    return redirect('home') 
-
+    return redirect('favoritos') #redirecciona a favoritos para verificar que se haya agregado correctamente
+    
 
 @login_required
 def deleteFavourite(request):
